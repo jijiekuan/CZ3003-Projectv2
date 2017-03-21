7ecb12d25274cbe979719ba663542a0b7c3e6860
Shide <shyde.foo@gmail.com>
2017-03-21 18:58:36 +0800

    first commit to local repo

---
+++ CZ3003-Projectv2-master/app/src/main/java/Common/Shared.java
@@ -0,0 +1,19 @@
+package Common;
+
+/**
+ * Created by zAdmin on 21/3/2017.
+ */
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+
+import com.snatik.matches.engine.Engine;
+import com.snatik.matches.events.EventBus;
+
+public class Shared {
+
+    public static Context context;
+    public static FragmentActivity activity;
+    public static Engine engine;
+    public static EventBus eventBus;
+}
